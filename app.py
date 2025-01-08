c74879675541658326422dda79e476edd467aaa8
kwonyongkwan <kyk3146@naver.com>
2025-01-08 16:41:33 +0900

    Updated Lambda function message

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
 import json
 
 def lambda_handler(event,context):
-        # http 요청을 처리하는 aws lambda 함수
-        
-        # 들어오는 이벤트 로그
-        print("Event:",event)
-
-        # 응답 메시지
-        response = {
+    return {
                 "statusCode":200,
-                "headers":{
-                        "Content-Type":"application/json"
-                },
-                "body":json.dumps({
-                        "message":"hello from AWS Lambda!",
-                        "input":event 
-                })
-        }
-
-        return response
+                "body":"Hello,AWS Lambda!"
+    }
